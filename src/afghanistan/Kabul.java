90130f67a2b46cb7d7174a769c7a203112b2bfbf
Israr <israr.akh03@gmail.com>
2023-05-21 18:47:15 -0400

    Just modified Kabul class

--- src/afghanistan/Kabul.java
+++ src/afghanistan/Kabul.java
@@ -5,5 +5,6 @@ public class Kabul {
 	public static void main(String[] args) {
 		
 		System.out.println("I love Kabul city.");
+		System.out.println("Kabul is the beautiful city in Afghanistan");
 	}
 }
