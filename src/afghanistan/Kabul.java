7efad2bec87e155864dbb0196eb350f7bb204828
Israr <israr.akh03@gmail.com>
2023-05-21 19:40:19 -0400

    Just modified Kabul class with the Age of it

--- src/afghanistan/Kabul.java
+++ src/afghanistan/Kabul.java
@@ -1,11 +1,14 @@
 package afghanistan;
 
 public class Kabul {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("I love Kabul city.");
 		System.out.println("Kabul is the beautiful city in Afghanistan");
 		System.out.println("Kabul is wonderful place for tourists");
+
+		int ageOfKanul = 2000;
+		System.out.println("Age of Kabul is: " + ageOfKanul);
 	}
 }
